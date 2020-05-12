bccb84b4e0bb6f1c98f6878ebe6023c1bf3e01ec
theSkyseS <bugaev.andrei55@gmail.com>
2020-05-12 18:10:04 +0600

    Добавлена ЛР к уроку 7.

---
+++ Lesson_7/Lesson_7/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Lesson_6;
+
+namespace Lesson_7
+{
+    public class Program
+    {
+        #region Task 1
+        public delegate double MyDel1(int[] a);
+
+        public static MyDel1 myDelegate1 = delegate (int[] a)
+        {
+            int sum = 0;
+            double result = 0;
+            foreach (int i in a)
+                sum += i;
+            result = (sum * 1.0) / a.Length;
+            return result;
+        };
+        #endregion
+
+        #region Task 2
+        public delegate int MyDel2(int x, int y, string oper);
+
+        public static MyDel2 myDelegate2 = delegate (int x, int y, string oper)//oper передаётся для тестов
+        {
+            //string oper = Console.ReadLine();
+            switch (oper)
+            {
+                case "+": return x + y;
+                case "-": return x - y;
+                default: Console.WriteLine("Введён неверный оператор"); return int.MinValue+1;
+            }
+            
+        };
+        #endregion
+
+        //сами события из задания 3 находятся в шестом уроке в computer.cs
+        static void Main(string[] args)
+        {
+            Computer computer = new Computer();
+            computer.UserAdded += delegate
+            {
+                if (computer.UserList.Contains("admin"))
+                    computer.UserList.Remove("admin");
+            };
+            computer.InvokeEvents();
+        }
+    }
+}
---
+++ Lesson_7/UnitTest/UnitTest.cs
@@ -0,0 +1,38 @@
+﻿using Lesson_7;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class UnitTest
+    {
+        [TestCase(new int[] { 0, 0, 0, 0 }, 0)]
+        [TestCase(new int[] { 1, 2, 3, 4, 5 }, 3)]
+        [TestCase(new int[] { 2, 4, 6, -4, 3 }, 2.2)]
+        [TestCase(new int[] { 0, 1, 3, 5 }, 2.25)]
+        [TestCase(new int[] { -1, -3, -5 }, -3)]
+        [TestCase(new int[] { 2, 6, 4, 25 }, 9.25)]
+        [TestCase(new int[] { 0, 2, 32, 5 }, 9.75)]
+        [TestCase(new int[] { 18, 250, -90, 24 }, 50.5)]
+        [TestCase(new int[] { -90, -90, -80, -25 }, -71.25)]
+        [TestCase(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, 1)]
+        public void Task1Test(int[] a, double expectedResult)
+        {
+            Assert.AreEqual(expectedResult, Program.myDelegate1.Invoke(a));
+        }
+
+        [TestCase(1, 2, "-", ExpectedResult = -1)]
+        [TestCase(1, 2, "+", ExpectedResult =  3)]
+        [TestCase(1, 2, "asdasd", ExpectedResult = int.MinValue+1)]
+        [TestCase(0, 0, "-", ExpectedResult = 0)]
+        [TestCase(0, 0, "+", ExpectedResult = 0)]
+        [TestCase(25, 5, "-", ExpectedResult = 20)]
+        [TestCase(-90, -70, "-", ExpectedResult = -20)]
+        [TestCase(-90, -70, "+", ExpectedResult = -160)]
+        [TestCase(500, 120, "-", ExpectedResult = 380)]
+        [TestCase(500, 120, "+", ExpectedResult = 620)]
+        public int Task2Test(int x, int y,string oper)
+        {
+            return Program.myDelegate2.Invoke(x, y, oper);
+        }
+    }
+}
